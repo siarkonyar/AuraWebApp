581bed226003f2b2ca881b8c26efac0ba649411c
siarkonyar <siarkonyar@gmail.com>
2021-08-23 20:46:50 +0300

    navbar done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,47 @@ const optionsContainer = document.querySelector(".options-container");
 
 const optionsList = document.querySelectorAll(".option");
 
+function addActiveClass(item){
+    item.classList.add("active");
+}
+function removeActiveClass(item){
+    item.classList.remove("active");
+}
+function toggleActiveClass(item){
+    item.classList.toggle("active");
+}
+
 selected.addEventListener("click",function(){
-    optionsContainer.classList.toggle("active");
+    toggleActiveClass(optionsContainer);
+    toggleActiveClass(selected);
 });
 
 optionsList.forEach(function(item){
     item.addEventListener("click",function(){
         selected.querySelector(".selected-language").innerHTML = item.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
+        removeActiveClass(optionsContainer);
+        removeActiveClass(selected);
     });
+});
+
+const categoriesDropdown = document.querySelector(".categories-dropdown");
+const categoryList = document.querySelector(".category-list");
+
+categoriesDropdown.addEventListener("mouseover",function(item){
+    addActiveClass(categoryList);
+    addActiveClass(categoriesDropdown.querySelector(".nav-dropdown-text"));
+})
+categoriesDropdown.addEventListener("mouseleave",function(item){
+    removeActiveClass(categoryList);
+    removeActiveClass(categoriesDropdown.querySelector(".nav-dropdown-text"));
+})
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".navbar-nav");
+const language = document.querySelector(".select-box");
+
+hamburger.addEventListener("click",function(item){
+    hamburger.classList.toggle("open");
+    toggleActiveClass(nav);
+    toggleActiveClass(language);
 });
